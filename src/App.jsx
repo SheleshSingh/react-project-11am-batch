543ab129e5f8ef56eb42e7746641ca60c7b1de89
SheleshSingh <sheleshsingh755@gmail.com>
2026-01-19 11:26:04 +0530

    start new project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './componet/Header'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/componet/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='flex items-center justify-between'>
+            <h1 className='text-2xl font-semibold'><span className='text-red-500 font-medium'>T</span>ecknosters</h1>
+        </div>
+    )
+}
+
+export default Header
