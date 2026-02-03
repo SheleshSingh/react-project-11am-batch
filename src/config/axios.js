739b8f1bf359394525999f0e224215ffd32a8f2d
dharmesh <sagardharmesh9@gmail.com>
2026-02-03 12:10:59 +0530

    bfjksgjkfsg

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://69731167b5f46f8b5826076e.mockapi.io/",
+  baseURL: "https://68ede399df2025af78016bd3.mockapi.io/",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
